be57500ef32d09f5bb5720bd1fd9fe91c75b1ad4
William Hart <william.hart@maths.ox.ac.uk>
2023-10-31 18:05:36 +0000

    Main accessor methods now work on multiple data variables at once where possible

--- climepi/_core.py
+++ climepi/_core.py
@@ -19,7 +19,7 @@ class ClimEpiDatasetAccessor:
 
     def __init__(self, xarray_obj):
         self._obj = xarray_obj
-        self._modes = None
+        self._modes = {}
 
     @property
     def modes(self):
@@ -36,12 +36,12 @@ class ClimEpiDatasetAccessor:
 
     @modes.setter
     def modes(self, modes_in):
-        assert isinstance(modes_in, dict)
-        assert all(key in ["spatial", "temporal", "ensemble"] for key in modes_in)
-        assert all(key in modes_in for key in ["spatial", "temporal", "ensemble"])
-        assert modes_in["spatial"] in ["global"]
-        assert modes_in["temporal"] in ["monthly", "annual"]
-        assert modes_in["ensemble"] in ["ensemble", "single_run", "stats"]
+        # assert isinstance(modes_in, dict)
+        # assert all(key in ["spatial", "temporal", "ensemble"] for key in modes_in)
+        # assert all(key in modes_in for key in ["spatial", "temporal", "ensemble"])
+        # assert modes_in["spatial"] in ["global"]
+        # assert modes_in["temporal"] in ["monthly", "annual"]
+        # assert modes_in["ensemble"] in ["ensemble", "single_run", "stats"]
         self._modes = modes_in
 
     def annual_mean(self, data_var=None):
@@ -50,29 +50,30 @@ class ClimEpiDatasetAccessor:
 
         Parameters
         ----------
-        data_var : str, optional
-            Name of the data variable to compute the annual mean for. If not
-            provided, the function will attempt to automatically select a
-            suitable variable.
+        data_var : str or list, optional
+            Name(s) of the data variable(s) to compute the annual mean for. If not
+            provided, all non-bound data variables will be used.
 
         Returns
         -------
         xarray.Dataset
             A new dataset containing the annual mean of the selected data
-            variable.
+            variable(s).
         """
-        data_var = self._auto_select_data_var(data_var)
-        if "time" not in self._obj.sizes:
-            raise ValueError("Annual mean only defined for time series.")
-        if np.issubdtype(self._obj[data_var].dtype, np.integer) or np.issubdtype(
+        data_var = self._auto_select_data_var(data_var, allow_multiple=True)
+        if isinstance(data_var, list):
+            ds_m_list = [self.annual_mean(data_var_curr) for data_var_curr in data_var]
+            ds_m = xr.merge(ds_m_list)
+        elif np.issubdtype(self._obj[data_var].dtype, np.integer) or np.issubdtype(
             self._obj[data_var].dtype, np.integer
         ):
-            # Workaround for bug in xcdat group-average using integer or
+            # Workaround for bug in xcdat group_average using integer or
             # boolean data types
             ds_copy = self._obj.copy()
             ds_copy[data_var] = ds_copy[data_var].astype("float64")
-            return ds_copy.climepi.annual_mean(data_var)
-        ds_m = self._obj.temporal.group_average(data_var, freq="year")
+            ds_m = ds_copy.climepi.annual_mean(data_var)
+        else:
+            ds_m = self._obj.temporal.group_average(data_var, freq="year")
         ds_m.climepi.modes = dict(self.modes, temporal="annual")
         return ds_m
 
@@ -82,21 +83,20 @@ class ClimEpiDatasetAccessor:
 
         Parameters
         ----------
-        data_var : str, optional
-            Name of the data variable to compute the ensemble mean for. If not
-            provided, the function will attempt to automatically select a
-            suitable variable.
+        data_var : str or list, optional
+            Name(s) of the data variable(s) to compute the ensemble mean for. If not
+            provided, all non-bound data variables will be used.
 
         Returns
         -------
         xarray.Dataset
             A new dataset containing the ensemble mean of the selected data
-            variable.
+            variable(s).
         """
-        data_var = self._auto_select_data_var(data_var)
+        data_var = self._auto_select_data_var(data_var, allow_multiple=True)
         ds_m = xr.Dataset(attrs=self._obj.attrs)
         ds_m[data_var] = self._obj[data_var].mean(dim="realization")
-        ds_m[data_var].attrs = self._obj[data_var].attrs
+        ds_m.climepi.copy_var_attrs_from(self._obj, var=data_var)
         ds_m.climepi.copy_bnds_from(self._obj)
         ds_m.climepi.modes = dict(self.modes, ensemble="stats")
         return ds_m
@@ -107,10 +107,9 @@ class ClimEpiDatasetAccessor:
 
         Parameters
         ----------
-        data_var : str, optional
-            Name of the data variable to compute the ensemble percentiles for.
-            If not provided, the function will attempt to automatically select
-            a suitable variable.
+        data_var : str or list, optional
+            Name(s) of the data variable(s) to compute the ensemble percentiles for.
+            If not provided, all non-bound data variables will be used.
         values : list of float, optional
             Percentiles to compute. Defaults to [5, 50, 95] if not provided.
         **kwargs : dict, optional
@@ -121,16 +120,16 @@ class ClimEpiDatasetAccessor:
         -------
         xarray.Dataset
             A new dataset containing the ensemble percentiles of the selected
-            data variable.
+            data variable(s).
         """
         if values is None:
             values = [5, 50, 95]
-        data_var = self._auto_select_data_var(data_var)
+        data_var = self._auto_select_data_var(data_var, allow_multiple=True)
         ds_p = xr.Dataset(attrs=self._obj.attrs)
         ds_p[data_var] = xclim.ensembles.ensemble_percentiles(
             self._obj[data_var], values, split=False, **kwargs
         ).rename({"percentiles": "percentile"})
-        ds_p[data_var].attrs = self._obj[data_var].attrs
+        ds_p.climepi.copy_var_attrs_from(self._obj, var=data_var)
         ds_p.climepi.copy_bnds_from(self._obj)
         ds_p.climepi.modes = dict(self.modes, ensemble="stats")
         return ds_p
@@ -142,10 +141,9 @@ class ClimEpiDatasetAccessor:
 
         Parameters
         ----------
-        data_var : str, optional
-            Name of the data variable to compute the ensemble statistics for.
-            If not provided, the function will attempt to automatically select
-            a suitable variable.
+        data_var : str or list, optional
+            Name(s) of the data variable(s) to compute the ensemble statistics for.
+            If not provided, all non-bound data variables will be used.
         **kwargs : dict, optional
             Additional keyword arguments to pass to
             xclim.ensembles.ensemble_mean_std_max_min.
@@ -154,25 +152,34 @@ class ClimEpiDatasetAccessor:
         -------
         xarray.Dataset
             A new dataset containing the computed ensemble statistics for the
-            selected data variable.
+            selected data variable(s).
         """
-        data_var = self._auto_select_data_var(data_var)
-        ds_stat_xclim = xclim.ensembles.ensemble_mean_std_max_min(
-            self._obj[data_var].to_dataset(), **kwargs
-        )
-        stat_list = ["mean", "std", "max", "min"]
-        stat_list_xclim = [data_var + "_" + stat_list[i] for i in range(len(stat_list))]
-        stat_list_xclim[1] += "ev"
-        ds_stat = xr.Dataset(attrs=self._obj.attrs)
-        da_stat_xclim_list = [
-            ds_stat_xclim[stat_list_xclim[i]]
-            .rename(data_var)
-            .expand_dims(dim={"ensemble_statistic": [stat_list[i]]}, axis=-1)
-            for i in range(len(stat_list))
-        ]
-        ds_stat[data_var] = xr.concat(da_stat_xclim_list, dim="ensemble_statistic")
-        ds_stat[data_var].attrs = self._obj[data_var].attrs
-        ds_stat.climepi.copy_bnds_from(self._obj)
+        data_var = self._auto_select_data_var(data_var, allow_multiple=True)
+        if isinstance(data_var, list):
+            ds_stat_list = [
+                self.ensemble_mean_std_max_min(data_var_curr, **kwargs)
+                for data_var_curr in data_var
+            ]
+            ds_stat = xr.merge(ds_stat_list)
+        else:
+            ds_stat_xclim = xclim.ensembles.ensemble_mean_std_max_min(
+                self._obj[data_var].to_dataset(), **kwargs
+            )
+            stat_list = ["mean", "std", "max", "min"]
+            stat_list_xclim = [
+                data_var + "_" + stat_list[i] for i in range(len(stat_list))
+            ]
+            stat_list_xclim[1] += "ev"
+            ds_stat = xr.Dataset(attrs=self._obj.attrs)
+            da_stat_xclim_list = [
+                ds_stat_xclim[stat_list_xclim[i]]
+                .rename(data_var)
+                .expand_dims(dim={"ensemble_statistic": [stat_list[i]]}, axis=-1)
+                for i in range(len(stat_list))
+            ]
+            ds_stat[data_var] = xr.concat(da_stat_xclim_list, dim="ensemble_statistic")
+            ds_stat.climepi.copy_var_attrs_from(self._obj, var=data_var)
+            ds_stat.climepi.copy_bnds_from(self._obj)
         ds_stat.climepi.modes = dict(self.modes, ensemble="stats")
         return ds_stat
 
@@ -182,10 +189,9 @@ class ClimEpiDatasetAccessor:
 
         Parameters
         ----------
-        data_var : str, optional
-            Name of the data variable to compute the ensemble statistics for.
-            If not provided, the function will attempt to automatically select
-            a suitable variable.
+        data_var : str or list, optional
+            Name(s) of the data variable(s) to compute the ensemble statistics for.
+            If not provided, all non-bound data variables will be used.
         conf_level : float, optional
             Confidence level for computing ensemble percentiles.
         **kwargs : dict, optional
@@ -196,9 +202,9 @@ class ClimEpiDatasetAccessor:
         -------
         xarray.Dataset
             A new dataset containing the computed ensemble statistics for the
-            selected data variable.
+            selected data variable(s).
         """
-        data_var = self._auto_select_data_var(data_var)
+        data_var = self._auto_select_data_var(data_var, allow_multiple=True)
         ds_msmm = self._obj.climepi.ensemble_mean_std_max_min(data_var, **kwargs)
         ds_mci = self._obj.climepi.ensemble_percentiles(
             data_var, [50 - conf_level / 2, 50, 50 + conf_level / 2], **kwargs
@@ -332,6 +338,23 @@ class ClimEpiDatasetAccessor:
         p_central = da_central.hvplot.line(**kwargs_hv_central)
         return p_central * p_ci
 
+    def copy_var_attrs_from(self, ds_from, var):
+        """
+        Copies the attributes for a variable from another xarray dataset (ds_from) to
+        this one.
+
+        Parameters
+        ----------
+        ds_from : xarray.Dataset
+            The dataset to copy the variable attributes from.
+        vars : str or list
+            The name of the variable to copy the attributes for.
+        """
+        if isinstance(var, list):
+            for var_curr in var:
+                self.copy_var_attrs_from(ds_from, var_curr)
+        self._obj[var].attrs = ds_from[var].attrs
+
     def copy_bnds_from(self, ds_from):
         """
         Copies the latitude, longitude, and time bounds from another xarray
@@ -367,17 +390,18 @@ class ClimEpiDatasetAccessor:
         ]
         return non_bnd_data_vars
 
-    def _auto_select_data_var(self, data_var):
+    def _auto_select_data_var(self, data_var_in, allow_multiple=False):
         # Method for obtaining the name of the data variable in the xarray
         # dataset, if only one is present (alongside latitude, longitude, and
         # time bounds).
-        if data_var is None:
-            non_bnd_data_vars = self.get_non_bnd_data_vars()
-            if len(non_bnd_data_vars) == 1:
-                data_var = non_bnd_data_vars[0]
-            else:
-                raise ValueError(
-                    """Multiple data variables present. The data variable to
-                    use must be specified."""
-                )
-        return data_var
+        if data_var_in is not None and (isinstance(data_var_in, str) or allow_multiple):
+            return data_var_in
+        non_bnd_data_vars = self.get_non_bnd_data_vars()
+        if len(non_bnd_data_vars) == 1:
+            return non_bnd_data_vars[0]
+        if allow_multiple:
+            return non_bnd_data_vars
+        raise ValueError(
+            """Multiple data variables present. The data variable to use must be
+            specified."""
+        )
