ef102e5ff2bb2733d4dd39cdf829341747310a2a
William Hart <william.hart@maths.ox.ac.uk>
2024-02-06 17:58:35 +0000

    Update docstrings; move year filtering to data finding method

--- climepi/climdata/_isimip.py
+++ climepi/climdata/_isimip.py
@@ -15,6 +15,23 @@ geolocator = Nominatim(user_agent="climepi")
 
 
 class ISIMIPDataGetter(ClimateDataGetter):
+
+    """
+    Class for accessing and downloading ISIMIP data from the ISIMIP repository
+    (https://www.isimip.org/outputdata/isimip-repository/). Available
+    years that can be specified in the `subset` argument of the class constructor range
+    from 2015 to 2101, and a single realization (here labelled as 0) is available for
+    a variety of emissions scenarios ("ssp126", "ssp245", "ssp370", and "ssp585") and
+    models ("gfdl-esm4", "ipsl-cm6a-lr", "mpi-esm1-2-hr", "mri-esm2-0", "ukesm1-0-ll",
+    "canesm5", "cnrm-cm6-1", "cnrm-esm2-1", "ec-earth3", and "miroc6"); note that the
+    "ssp245" scenario is only available for some models. The data must be downloaded
+    (i.e., `download=True` option in the `get_data` method) before it can be opened and
+    processed.
+
+    See the base class (`climepi.climdata._data_getter_class.ClimateDataGetter`) for
+    further details.
+    """
+
     data_source = "isimip"
     available_years = np.arange(2015, 2101)
     available_scenarios = ["ssp126", "ssp245", "ssp370", "ssp585"]
@@ -33,12 +50,19 @@ class ISIMIPDataGetter(ClimateDataGetter):
     available_realizations = [0]
 
     def __init__(self, *args, **kwargs):
+        # Extends the base class constructor to include the _client_results attribute,
+        # which stores the results of the client requests to the ISIMIP repository.
         super().__init__(*args, **kwargs)
         self._client_results = None
 
     def _find_remote_data(self):
+        # Use the ISIMIPClient to find the available data for the requested models,
+        # scenarios and years, and store a list of results (each entry of which
+        # comprises a dictionary containing details of a single data file) in the
+        # _client_results attribute.
         scenarios = self._subset["scenarios"]
         models = self._subset["models"]
+        years = self._subset["years"]
         response = ISIMIPClient().files(
             simulation_round="ISIMIP3b",
             climate_variable=["tas", "pr"],
@@ -49,12 +73,25 @@ class ISIMIPDataGetter(ClimateDataGetter):
         while response["next"] is not None:
             response = ISIMIPClient(data_url="").get(response["next"])
             client_results.extend(response["results"])
+        # Filter the results to only include files that are within the requested years
+        # Get paths for files that are within the requested years
+        client_results = [
+            result
+            for result in client_results
+            if any(
+                (
+                    result["specifiers"]["start_year"]
+                    <= year
+                    <= result["specifiers"]["end_year"]
+                    for year in years
+                )
+            )
+        ]
         self._client_results = client_results
 
     def _subset_remote_data(self):
-        # Subset the remotely opened dataset to the requested years, realizations and
-        # location(s), and store the subsetted dataset in the _ds attribute.
-        years = self._subset["years"]
+        # Request server-side subsetting of the data to the requested location(s) and
+        # update the _client_results attribute with the results of the subsetting.
         loc_str = self._subset["loc_str"]
         lon_range = self._subset["lon_range"]
         lat_range = self._subset["lat_range"]
@@ -73,22 +110,8 @@ class ISIMIPDataGetter(ClimateDataGetter):
             if lat_range is None:
                 lat_range = [-90, 90]
             bbox = [lat_range[0], lat_range[1], lon_range[0], lon_range[1]]
-        # Get paths for files that are within the requested years
-        client_file_paths = [file["path"] for file in client_results]
-        client_file_start_years = [
-            file["specifiers"]["start_year"] for file in client_results
-        ]
-        client_file_end_years = [
-            file["specifiers"]["end_year"] for file in client_results
-        ]
-        client_file_paths = [
-            path
-            for path, file_start_year, file_end_year in zip(
-                client_file_paths, client_file_start_years, client_file_end_years
-            )
-            if any((file_start_year <= year <= file_end_year for year in years))
-        ]
         # Request server to subset the data
+        client_file_paths = [file["path"] for file in client_results]
         paths_by_cutout_request = [
             client_file_paths[i : i + 500]
             for i in range(0, len(client_file_paths), 500)
@@ -107,7 +130,7 @@ class ISIMIPDataGetter(ClimateDataGetter):
                 ],
                 sep="\n",
             )
-            if all(job_status == "completed" for job_status in job_statuses):
+            if all(job_status == "finished" for job_status in job_statuses):
                 subsetting_completed = True
             else:
                 time.sleep(10)
@@ -130,7 +153,7 @@ class ISIMIPDataGetter(ClimateDataGetter):
                 path=temp_save_dir,
                 progressbar=True,
             )
-            if download_path_curr.suffix == ".zip":
+            if pathlib.Path(download_path_curr).suffix == ".zip":
                 with zipfile.ZipFile(download_path_curr, "r") as zip_ref:
                     temp_file_names_curr = [
                         name for name in zip_ref.namelist() if name[-3:] == ".nc"
