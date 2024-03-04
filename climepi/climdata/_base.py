548cb1d985ec9203407e133d22faf0a550d74adf
William Hart <william.hart@maths.ox.ac.uk>
2024-03-04 19:23:43 +0000

    Attempt to incorporate downloading data for multiple locations at once (not in a grid)

--- climepi/climdata/_base.py
+++ climepi/climdata/_base.py
@@ -1,3 +1,5 @@
+import xarray as xr
+
 from climepi.climdata._cesm import CESMDataGetter
 from climepi.climdata._isimip import ISIMIPDataGetter
 
@@ -51,6 +53,10 @@ def get_climate_data(
         Details of where downloaded data are saved to and accessed from are given in
         the CESMDataGetter class documentation.
     """
+    if "loc_str" in subset and isinstance(subset["loc_str"], list):
+        return _get_climate_data_loc_list(
+            data_source, frequency, subset, save_dir, download
+        )
     data_getter = _get_data_getter(
         data_source=data_source,
         frequency=frequency,
@@ -78,6 +84,14 @@ def get_climate_data_file_names(data_source="lens2", frequency="monthly", subset
         Dictionary of data subsetting options. See the docstring of `get_climate_data`
         for details.
     """
+    if "loc_str" in subset and isinstance(subset["loc_str"], list):
+        return [
+            file_name
+            for loc_str_curr in subset["loc_str"]
+            for file_name in get_climate_data_file_names(
+                data_source, frequency, {**subset, "loc_str": loc_str_curr}
+            )
+        ]
     data_getter = _get_data_getter(
         data_source=data_source,
         frequency=frequency,
@@ -95,3 +109,51 @@ def _get_data_getter(data_source, *args, **kwargs):
     else:
         raise ValueError(f"Data source '{data_source}' not supported.")
     return data_getter
+
+
+def _get_climate_data_loc_list(data_source, frequency, subset, save_dir, download):
+    import functools
+    import multiprocessing
+
+    if data_source == "isimip":
+        # Run for each location with a timeout to ensure server-side processing is
+        # requested for each location (can then cancel the Python process and restart
+        # once the server-side processing is complete).
+        for loc_str_curr in subset["loc_str"]:
+            subset_curr = {**subset, "loc_str": loc_str_curr}
+            target = functools.partial(
+                get_climate_data,
+                data_source,
+                frequency,
+                subset_curr,
+                save_dir,
+                download,
+            )
+            p = multiprocessing.Process(target=target)
+            p.start()
+            p.join(60)
+            if p.is_alive():
+                p.terminate()
+    ds_list = []
+    for loc_str_curr in subset["loc_str"]:
+        subset_curr = {**subset, "loc_str": loc_str_curr}
+        ds_curr = get_climate_data(
+            data_source, frequency, subset_curr, save_dir, download
+        )
+        ds_curr["location"] = [loc_str_curr]
+        for data_var in ds_curr.data_vars:
+            if data_var == "time_bnds":
+                continue
+            if "lon" in ds_curr[data_var].dims:
+                ds_curr[data_var] = ds_curr[data_var].squeeze("lon")
+            if "lat" in ds_curr[data_var].dims:
+                ds_curr[data_var] = ds_curr[data_var].squeeze("lat")
+            ds_curr[data_var] = ds_curr[data_var].expand_dims("location", axis=0)
+        if "lon" in ds_curr.dims:
+            ds_curr["lon"] = ds_curr["lon"].swap_dims({"lon": "location"})
+        if "lat" in ds_curr.dims:
+            ds_curr["lat"] = ds_curr["lat"].swap_dims({"lat": "location"})
+        ds_list.append(ds_curr)
+        # Set CF x and y coords?
+    ds = xr.concat(ds_list, dim="location", data_vars="minimal")
+    return ds
