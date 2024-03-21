a0782605911b5052374287596645a43c327c845e
William Hart <william.hart@maths.ox.ac.uk>
2024-03-21 16:54:06 +0000

    Renaming

--- climepi/epimod/__init__.py
+++ climepi/epimod/__init__.py
@@ -4,7 +4,7 @@ from climepi.epimod._examples import (  # noqa
     EXAMPLE_NAMES,
     get_example_model,
 )
-from climepi.epimod._epimod_base import (  # noqa
+from climepi.epimod._model_classes import (  # noqa
     EpiModDatasetAccessor,
     EpiModel,
     SuitabilityModel,
