7b5ccefd1251068d5ca2ddc3027a33745bccdd9a
William Hart <william.hart@maths.ox.ac.uk>
2024-05-01 17:24:56 +0100

    Unit tests for estimate_ensemble_stats and var_decomp

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,9 +1,15 @@
+"""
+Unit tests for the ClimEpiDatasetAccessor contained in the _core module of the climepi
+subpackage
+"""
+
 import cftime
 import numpy as np
 import numpy.testing as npt
 import pytest
 import xarray as xr
 import xarray.testing as xrt
+from scipy.stats import norm
 
 from climepi import ClimEpiDatasetAccessor  # noqa
 from climepi.testing.fixtures import generate_dataset, generate_epi_model
@@ -117,9 +123,17 @@ class TestTemporalGroupAverage:
             time_index_expected = ds_time_bnds_expected.get_index("time")
         elif frequency == "daily":
             temperature_values_expected = temperature_values_in
+            ds_time_bnds_expected = ds[["time_bnds"]]
             time_index_expected = ds.get_index("time")
         npt.assert_allclose(temperature_values_result, temperature_values_expected)
         assert time_index_result.equals(time_index_expected)
+        if frequency in ["monthly", "daily"]:
+            xrt.assert_allclose(
+                result["time_bnds"],
+                ds_time_bnds_expected["time_bnds"],
+            )
+        else:
+            assert "time_bnds" not in result
 
     def test_temporal_group_average_varlist(self, frequency):
         """
@@ -212,6 +226,10 @@ class TestMonthsSuitable:
         npt.assert_allclose(
             months_suitable_values_result, months_suitable_values_expected
         )
+        assert (
+            result.months_suitable.attrs["long_name"]
+            == "Months where suitability > 0.5"
+        )
 
     def test_months_suitable_var_names(self):
         """
@@ -291,6 +309,10 @@ class TestEnsembleStats:
         xrt.assert_allclose(
             result["temperature"].sel(ensemble_stat="upper", drop=True),
             ds["temperature"].quantile(0.8, dim="realization").drop_vars("quantile"),
+        )
+        xrt.assert_allclose(
+            result[["lon", "lat", "time", "lon_bnds", "lat_bnds", "time_bnds"]],
+            ds[["lon", "lat", "time", "lon_bnds", "lat_bnds", "time_bnds"]],
         )
 
     def test_ensemble_stats_varlist(self):
@@ -373,13 +395,144 @@ class TestEstimateEnsembleStats:
     """
 
     def test_estimate_ensemble_stats(self):
-        pass
+        """
+        Main test for the estimate_ensemble_stats method of the ClimEpiDatasetAccessor
+        class. This test is based on estimating ensemble stats from a temperature time
+        series made up of normally distributed noise added to a polynomial (matching
+        the underlying assumptions of the estimate_ensemble_stats method). This is
+        repeated multiple times to ensure there is no systematic bias in the estimated
+        ensemble statistics.
+        """
+        time = xr.cftime_range(start="2001-01-01", periods=10000, freq="MS")
+        days_from_start = cftime.date2num(time, "days since 2001-01-01")
+        mean_theoretical = (
+            0.0000000000123 * days_from_start**3
+            - 0.00000257 * days_from_start**2
+            - 0.326 * days_from_start
+            - 259.29
+        )
+        std_theoretical = 0.734
+        repeats = 100
+        mean_result_sum = np.zeros_like(mean_theoretical)
+        std_result_sum = np.zeros_like(mean_theoretical)
+        for repeat in range(repeats):
+            temperature_values_in = np.random.normal(
+                loc=mean_theoretical, scale=std_theoretical
+            )
+            ds = xr.Dataset(
+                {
+                    "temperature": ("time", temperature_values_in),
+                },
+                coords={"time": time},
+            )
+            ds["time"].encoding.update(calendar="standard")
+            result = ds.climepi.estimate_ensemble_stats(conf_level=80, polyfit_degree=3)
+            if repeat == 0:
+                # Just check for the first repeat that the results match those obtained
+                # by directly applying numpy's polynomial fitting method.
+                polyfit_for_expected_values = np.polynomial.Polynomial.fit(
+                    days_from_start, temperature_values_in, 3, full=True
+                )
+                mean_expected = polyfit_for_expected_values[0](days_from_start)
+                var_expected = polyfit_for_expected_values[1][0][0] / len(
+                    days_from_start
+                )
+                std_expected = var_expected**0.5
+                lower_expected = norm.ppf(0.1, loc=mean_expected, scale=std_expected)
+                upper_expected = norm.ppf(0.9, loc=mean_expected, scale=std_expected)
+                npt.assert_allclose(
+                    result["temperature"].sel(ensemble_stat="mean", drop=True).values,
+                    mean_expected,
+                )
+                npt.assert_allclose(
+                    result["temperature"].sel(ensemble_stat="std", drop=True).values,
+                    std_expected,
+                )
+                npt.assert_allclose(
+                    result["temperature"].sel(ensemble_stat="var", drop=True).values,
+                    var_expected,
+                )
+                npt.assert_allclose(
+                    result["temperature"].sel(ensemble_stat="lower", drop=True).values,
+                    lower_expected,
+                )
+                npt.assert_allclose(
+                    result["temperature"].sel(ensemble_stat="upper", drop=True).values,
+                    upper_expected,
+                )
+                xrt.assert_allclose(
+                    result[["lon", "lat", "time", "lon_bnds", "lat_bnds", "time_bnds"]],
+                    ds[["lon", "lat", "time", "lon_bnds", "lat_bnds", "time_bnds"]],
+                )
+            mean_result_sum += (
+                result["temperature"].sel(ensemble_stat="mean", drop=True).values
+            )
+            std_result_sum += (
+                result["temperature"].sel(ensemble_stat="std", drop=True).values
+            )
+        mean_result_avg = mean_result_sum / repeats
+        std_result_avg = std_result_sum / repeats
+        var_result_avg = std_result_avg**2
+        lower_result_avg = norm.ppf(0.1, loc=mean_result_avg, scale=std_result_avg)
+        upper_result_avg = norm.ppf(0.9, loc=mean_result_avg, scale=std_result_avg)
+        var_theoretical = std_theoretical**2
+        lower_theoretical = norm.ppf(0.1, loc=mean_theoretical, scale=std_theoretical)
+        upper_theoretical = norm.ppf(0.9, loc=mean_theoretical, scale=std_theoretical)
+        rtol_theoretical_match = 1e-2
+        npt.assert_allclose(
+            mean_result_avg,
+            mean_theoretical,
+            rtol=rtol_theoretical_match,
+        )
+        npt.assert_allclose(
+            std_result_avg,
+            std_theoretical,
+            rtol=rtol_theoretical_match,
+        )
+        npt.assert_allclose(
+            var_result_avg,
+            var_theoretical,
+            rtol=rtol_theoretical_match,
+        )
+        npt.assert_allclose(
+            lower_result_avg,
+            lower_theoretical,
+            rtol=rtol_theoretical_match,
+        )
+        npt.assert_allclose(
+            upper_result_avg,
+            upper_theoretical,
+            rtol=rtol_theoretical_match,
+        )
 
     def test_estimate_ensemble_stats_varlist(self):
-        pass
+        """
+        Test the estimate_ensemble_stats method of the ClimEpiDatasetAccessor class with
+        a list of data variables.
+        """
+        data_vars = ["temperature", "precipitation"]
+        ds = generate_dataset(data_var=data_vars, frequency="monthly")
+        ds["temperature"].values = np.random.rand(*ds["temperature"].shape)
+        ds["precipitation"].values = np.random.rand(*ds["precipitation"].shape)
+        result = ds.climepi.ensemble_stats()
+        for data_var in data_vars:
+            xrt.assert_allclose(
+                result[data_var],
+                ds[[data_var]].climepi.ensemble_stats()[data_var],
+            )
+            xrt.assert_allclose(
+                result[data_var],
+                ds.climepi.ensemble_stats(data_var)[data_var],
+            )
 
     def test_estimate_ensemble_stats_contains_realization(self):
-        ds_base = generate_dataset(data_var="temperature")
+        """
+        Test that the estimate_ensemble_stats method of the ClimEpiDatasetAccessor class
+        gives the expected result when the dataset contains a realization dimension with
+        length 1, or a non-dimensional realization coordinate, and raises an error when
+        the realization dimension has length greater than 1.
+        """
+        ds_base = generate_dataset(data_var="temperature", frequency="monthly")
         ds_base["temperature"].values = np.random.rand(*ds_base["temperature"].shape)
         ds1 = ds_base.copy()
         ds1["temperature"] = ds1["temperature"].expand_dims("realization")
@@ -395,3 +548,136 @@ class TestEstimateEnsembleStats:
         ds3["temperature"].values = np.random.rand(*ds3["temperature"].shape)
         with pytest.raises(ValueError):
             ds3.climepi.estimate_ensemble_stats()
+
+    def test_estimate_ensemble_stats_contains_singleton_coord(self):
+        """
+        Test that the estimate_ensemble_stats method of the ClimEpiDatasetAccessor class
+        correctly includes a non-dimensional singleton coordinate (other than time and
+        realization) in the output dataset.
+        """
+        ds = generate_dataset(data_var="temperature", frequency="monthly")
+        ds["hello there"] = "general kenobi"
+        ds = ds.set_coords("hello there")
+        result = ds.climepi.estimate_ensemble_stats()
+        assert "hello there" in result.coords
+        assert result["hello there"].values == "general kenobi"
+
+
+class TestVarDecomp:
+    """
+    Class defining tests for the var_decomp method of the ClimEpiDatasetAccessor class.
+    """
+
+    def test_var_decomp(self):
+        """
+        Main test for the var_decomp method of the ClimEpiDatasetAccessor class.
+        """
+        ds = generate_dataset(
+            data_var="temperature",
+            extra_dims={"scenario": 6, "model": 4, "realization": 9},
+        )
+        ds["temperature"].values = np.random.rand(*ds["temperature"].shape)
+        ds["temperature"].attrs.update(
+            units="Hello there", long_name="General Kenobi you are a bold one"
+        )
+        result = ds.climepi.var_decomp()
+        result_fractional = ds.climepi.var_decomp(fraction=True)
+        temperature_values = (
+            ds["temperature"]
+            .transpose("scenario", "model", "realization", "time", ...)
+            .values
+        )
+        var_total = np.var(temperature_values, axis=(0, 1, 2))
+        var_internal = np.mean(np.var(temperature_values, axis=2), axis=(0, 1))
+        var_model = np.mean(np.var(np.mean(temperature_values, axis=2), axis=1), axis=0)
+        var_scenario = np.var(np.mean(temperature_values, axis=(1, 2)), axis=0)
+        var_frac_internal = var_internal / var_total
+        var_frac_model = var_model / var_total
+        var_frac_scenario = var_scenario / var_total
+        npt.assert_allclose(
+            result["temperature"].sel(var_type="internal", drop=True).values,
+            var_internal,
+        )
+        npt.assert_allclose(
+            result["temperature"].sel(var_type="model", drop=True).values,
+            var_model,
+        )
+        npt.assert_allclose(
+            result["temperature"].sel(var_type="scenario", drop=True).values,
+            var_scenario,
+        )
+        npt.assert_allclose(
+            result_fractional["temperature"].sel(var_type="internal", drop=True).values,
+            var_frac_internal,
+        )
+        npt.assert_allclose(
+            result_fractional["temperature"].sel(var_type="model", drop=True).values,
+            var_frac_model,
+        )
+        npt.assert_allclose(
+            result_fractional["temperature"].sel(var_type="scenario", drop=True).values,
+            var_frac_scenario,
+        )
+        assert result["temperature"].attrs["units"] == "(Hello there)²"
+        assert (
+            result["temperature"].attrs["long_name"]
+            == "Variance of general Kenobi you are a bold one"
+        )
+        assert "units" not in result_fractional["temperature"].attrs
+        assert (
+            result_fractional["temperature"].attrs["long_name"]
+            == "Fraction of variance"
+        )
+        xrt.assert_allclose(
+            result[["lon", "lat", "time", "lon_bnds", "lat_bnds", "time_bnds"]],
+            ds[["lon", "lat", "time", "lon_bnds", "lat_bnds", "time_bnds"]],
+        )
+
+    def test_var_decomp_varlist(self):
+        """
+        Test the var_decomp method of the ClimEpiDatasetAccessor class with a list of
+        data variables.
+        """
+        data_vars = ["temperature", "precipitation"]
+        ds = generate_dataset(data_var=data_vars, frequency="monthly")
+        ds["temperature"].values = np.random.rand(*ds["temperature"].shape)
+        ds["precipitation"].values = np.random.rand(*ds["precipitation"].shape)
+        result = ds.climepi.var_decomp()
+        for data_var in data_vars:
+            xrt.assert_allclose(
+                result[data_var],
+                ds[[data_var]].climepi.var_decomp()[data_var],
+            )
+            xrt.assert_allclose(
+                result[data_var],
+                ds.climepi.var_decomp(data_var)[data_var],
+            )
+
+    def test_var_decomp_single_scenario_model(self):
+        """
+        Test the var_decomp method of the ClimEpiDatasetAccessor class with datasets
+        containing a single scenario and model.
+        """
+        ds1 = generate_dataset(data_var="temperature", extra_dims={"realization": 9})
+        ds1["temperature"].values = np.random.rand(*ds1["temperature"].shape)
+        ds2 = ds1.copy()
+        ds2["temperature"] = ds2["temperature"].expand_dims(["model", "scenario"])
+        ds3 = ds1.copy()
+        ds3["model"] = "googly"
+        ds3["scenario"] = "flipper"
+        ds3 = ds3.set_coords(["model", "scenario"])
+        result1 = ds1.climepi.var_decomp()
+        result2 = ds2.climepi.var_decomp()
+        result3 = ds3.climepi.var_decomp()
+        xrt.assert_allclose(result1, result2)
+        xrt.assert_allclose(result1, result3)
+        xrt.assert_allclose(
+            result1["temperature"].sel(var_type="internal", drop=True),
+            ds1["temperature"].var(dim="realization"),
+        )
+        npt.assert_allclose(
+            result1["temperature"].sel(var_type="model", drop=True).values, 0
+        )
+        npt.assert_allclose(
+            result1["temperature"].sel(var_type="scenario", drop=True).values, 0
+        )
