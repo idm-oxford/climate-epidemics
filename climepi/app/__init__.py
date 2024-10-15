bcdfb7824b04b6c7d8eba0a4487862cf8b303663
William Hart <william.hart@maths.ox.ac.uk>
2024-10-15 11:31:24 +0100

    Move app to separate repo

--- climepi/app/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Subpackage providing a browser-based front-end application.
-"""
-
-from climepi.app._run_app import run_app  # noqa
--- climepi/app/__main__.py
+++
@@ -1,5 +0,0 @@
-"""Entry point for the application. Run with ``python -m climepi.app``"""
-
-from climepi.app import run_app
-
-run_app()
