18dc52d1c3550b30c3f4a6222fb6e136b59c3c3d
William Hart <william.hart@maths.ox.ac.uk>
2024-10-30 18:43:27 +0000

    Aesthetic changes

--- climepi/app/cluster.py
+++ climepi/app/cluster.py
@@ -1,5 +1,5 @@
 """
-Script to start a Dask distributed cluster to be used in the application.
+Script to start a Dask local cluster to be used in the application.
 
 As per Panel recommendation (https://panel.holoviz.org/how_to/concurrency/dask.html),
 this script should be used to start the cluster separately from the application
@@ -13,6 +13,7 @@ from climepi.app import DASK_SCHEDULER_PORT
 if __name__ == "__main__":
     cluster = LocalCluster(scheduler_port=DASK_SCHEDULER_PORT)
     print(
-        f"Dask cluster started. Check the Dask dashboard at {cluster.dashboard_link}."
+        "Dask local cluster started. "
+        f"Check the Dask dashboard at {cluster.dashboard_link}."
     )
     input()
