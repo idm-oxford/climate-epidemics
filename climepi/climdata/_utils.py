b3d9ff190a587aafb0c2d8428dc0d570dad883b1
will-s-hart <william.hart@maths.ox.ac.uk>
2025-12-29 18:45:58 +0000

    Refactor version retrieval in _get_data_version function

--- climepi/climdata/_utils.py
+++ climepi/climdata/_utils.py
@@ -2,13 +2,15 @@
 
 import pooch
 
-from climepi._version import get_versions
+from climepi._version import get_version_dict
 
 FALLBACK_BRANCH = "main"
 
 
 def _get_data_version() -> str:
-    version = pooch.check_version(get_versions()["version"], fallback=FALLBACK_BRANCH)
+    version = pooch.check_version(
+        get_version_dict()["version"], fallback=FALLBACK_BRANCH
+    )
     if version != FALLBACK_BRANCH:
         version = "v" + version
     return version
